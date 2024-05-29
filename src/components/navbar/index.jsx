236dbbe32e77b8e8cfbda7ef438498d8b9e4d718
theophilusnuer <tphilus003@gmail.com>
2024-05-29 15:55:44 +0000

    linking

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { HashLink } from 'react-router-hash-link';
-import LoginForm from '../../pages/login';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,20 +19,20 @@ const Navbar = () => {
         <div className="hidden md:flex space-x-6 items-center">
           <a href="/" className="">Home</a>
           <HashLink to='/#aboutus'>
-          <p className="">About Us</p>
+            <p className="">About Us</p>
           </HashLink>
           <div className="relative group">
             <button className="">Categories</button>
             <div className="absolute hidden group-hover:block bg-white text-black mt-2 rounded shadow-lg">
-              <a href="#category1" className="block px-4 py-2 hover:bg-gray-200">Laboratory</a>
-              <a href="#category2" className="block px-4 py-2 hover:bg-gray-200">Surgical</a>
-              <a href="#category3" className="block px-4 py-2 hover:bg-gray-200">Dental</a>
-              <a href="#category4" className="block px-4 py-2 hover:bg-gray-200">Ear, Nose & Throat</a>
-              <a href="#category5" className="block px-4 py-2 hover:bg-gray-200">Pediatrics</a>
+              <Link to="/category/Laboratory" className="block px-4 py-2 hover:bg-gray-200">Laboratory</Link>
+              <Link to="/category/Surgical" className="block px-4 py-2 hover:bg-gray-200">Surgical</Link>
+              <Link to="/category/Dental" className="block px-4 py-2 hover:bg-gray-200">Dental</Link>
+              <Link to="/category/Ear, Nose & Throat" className="block px-4 py-2 hover:bg-gray-200">Ear, Nose & Throat</Link>
+              <Link to="/category/Pediatrics" className="block px-4 py-2 hover:bg-gray-200">Pediatrics</Link>
             </div>
           </div>
           <HashLink to='/#footer'>
-          <p className="">Contact</p>
+            <p className="">Contact</p>
           </HashLink>
           <a
             href="#signin"
@@ -60,19 +61,19 @@ const Navbar = () => {
       </div>
       {isOpen && (
         <div className="md:hidden bg-white mt-2 p-4 space-y-2">
-          <a href="#home" className="block  p-2">Home</a>
+          <a href="/" className="block  p-2">Home</a>
           <a href="#aboutus" className="block  p-2">About Us</a>
           <div className="relative">
             <button className="block  p-2">Categories</button>
             <div className="bg-white text-black rounded shadow-lg mt-2">
-              <a href="#category1" className="block px-4 py-2 hover:bg-gray-200">Category 1</a>
-              <a href="#category2" className="block px-4 py-2 hover:bg-gray-200">Category 2</a>
-              <a href="#category3" className="block px-4 py-2 hover:bg-gray-200">Category 3</a>
-              <a href="#category4" className="block px-4 py-2 hover:bg-gray-200">Category 4</a>
-              <a href="#category5" className="block px-4 py-2 hover:bg-gray-200">Category 5</a>
+              <Link to="/category/Laboratory" className="block px-4 py-2 hover:bg-gray-200">Laboratory</Link>
+              <Link to="/category/Surgical" className="block px-4 py-2 hover:bg-gray-200">Surgical</Link>
+              <Link to="/category/Dental" className="block px-4 py-2 hover:bg-gray-200">Dental</Link>
+              <Link to="/category/Ear, Nose & Throat" className="block px-4 py-2 hover:bg-gray-200">Ear, Nose & Throat</Link>
+              <Link to="/category/Pediatrics" className="block px-4 py-2 hover:bg-gray-200">Pediatrics</Link>
             </div>
           </div>
-          <a href="#contact" className="block  p-2">Contact</a>
+          <a href="/#footer" className="block  p-2">Contact</a>
           <a
             href="#register"
             className="block bg-blue-500 text-white px-3 py-1 text-sm rounded transition-colors duration-300 hover:bg-white hover:text-black hover:border hover:border-blue-500"
